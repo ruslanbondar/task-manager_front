22ec7ff3f84a07f8727d760511a774fcbd1faea1
Ruslan Bondar <mailofbondik@gmail.com>
2020-07-02 20:17:13 +0300

    baseURL change

--- src/api/api.js
+++ src/api/api.js
@@ -1,83 +1,86 @@
-import * as axios from "axios";
+import * as axios from 'axios'
 
-axios.defaults.baseURL = "http://localhost:3001/";
+// axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = 'https://task-manager-magisoft.herokuapp.com/'
 
-const token = localStorage.getItem("user-token");
+const token = localStorage.getItem('user-token')
 if (token) {
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
 }
 
 export const userAPI = {
   async getUsers() {
-    const res = await axios.get("users");
-    return res.data;
+    const res = await axios.get('users')
+    return res.data
   },
   async getLoggedInUser() {
-    const res = await axios.get("users/me");
-    return res.data;
+    const res = await axios.get('users/me')
+    return res.data
   },
   async postUser(data) {
-    const res = await axios.post("users", data);
-    return res.data;
+    const res = await axios.post('users', data)
+    return res.data
   },
   async loginUser(data) {
-    const res = await axios.post("users/login", data);
-    localStorage["user-token"] = res.data.token;
-    axios.defaults.headers.common["Authorization"] = `Bearer ${res.data.token}`;
-    return res.data;
+    const res = await axios.post('users/login', data)
+    localStorage['user-token'] = res.data.token
+    axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.token}`
+    return res.data
   },
   async logoutUser() {
-    const res = await axios.post('users/logout');
-    delete axios.defaults.headers.common["Authorization"];
-    localStorage.removeItem("user-token");
-    return res.data;
+    const res = await axios.post('users/logout')
+    delete axios.defaults.headers.common['Authorization']
+    localStorage.removeItem('user-token')
+    return res.data
   },
   async updateUser(data) {
-    const res = await axios.patch("users/me", data);
-    return res.data;
+    const res = await axios.patch('users/me', data)
+    return res.data
   },
   async deleteUser() {
-    const res = await axios.delete("users/me");
-    delete axios.defaults.headers.common["Authorization"];
-    localStorage.removeItem("user-token");
-    return res.data;
+    const res = await axios.delete('users/me')
+    delete axios.defaults.headers.common['Authorization']
+    localStorage.removeItem('user-token')
+    return res.data
   },
   async addPhoto(newData) {
-    const formData = new FormData();
+    const formData = new FormData()
     for (const key in newData) {
-      formData.append(key, newData[key]);
+      formData.append(key, newData[key])
     }
     return await axios.post('users/me/avatar', formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
-    });
+    })
   },
   async getPhoto() {
-    const res = await axios.get(`users/me/avatar`);
-    return res.data;
-  }
-};
+    const res = await axios.get(`users/me/avatar`)
+    return res.data
+  },
+}
 
 export const taskAPI = {
   async getTasks(page, done, date, skip) {
-    const res = await axios.get(`tasks?limit=7&skip=${skip}&sortBy=createdAt:${date}&page=${page}&completed=${done}`);
-    return res.data;
+    const res = await axios.get(
+      `tasks?limit=7&skip=${skip}&sortBy=createdAt:${date}&page=${page}&completed=${done}`
+    )
+    return res.data
   },
   async getTasksById(id) {
-    const res = await axios.get(`tasks?id=${id}`);
-    return res.data;
+    const res = await axios.get(`tasks?id=${id}`)
+    return res.data
   },
   async postTask(newData) {
-    const res = await axios.post('tasks', newData);
-    return res.data;
+    const res = await axios.post('tasks', newData)
+    return res.data
   },
   async updateTask(newData, id) {
-    const res = await axios.patch(`tasks/${id}`, newData);
-    return res.data;
+    const res = await axios.patch(`tasks/${id}`, newData)
+    return res.data
   },
   async deleteTask(id) {
-    const res = await axios.delete(`tasks/${id}`);
-    return res.data;
-  }
-};
+    const res = await axios.delete(`tasks/${id}`)
+    return res.data
+  },
+}
